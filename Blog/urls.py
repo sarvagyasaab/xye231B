580d6afafc78bc5f11624cd65f688e12bdc956dd
SarvagyaKumar <sarvagyakrcs@gmail.com>
2023-08-06 20:48:14 +0530

    created url endpoints

---
+++ Blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_posts, name='blog-posts'),
+    # path('create_post/', views.create_post, name='blog-create-posts'),
+]
