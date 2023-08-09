cd0207911f222abbb8d095b773a14d36d4380d0d
SarvagyaKumar <sarvagyakrcs@gmail.com>
2023-08-09 22:17:20 +0530

    Added Database Models `Friendship`, `FriendRequest`

--- Blog/models.py
+++ Blog/models.py
@@ -24,3 +24,36 @@ class Comments(models.Model):
         ]
 
 
+class Friendship(models.Model):
+    friendship_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_friendships')
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend_friendships')
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+        ('unfriended', 'Unfriended'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.friend.username}: {self.status}"
+
+class FriendRequest(models.Model):
+    request_id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_requests')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_requests')
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=('pending', 'Pending'))
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender.username} to {self.receiver.username}: {self.status}"
+
+
+
